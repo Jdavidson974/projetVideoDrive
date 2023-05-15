73176fbe391ecb2549e44d2aed5aa0c1bb619e95
Jdavidson <jd9743101@gmail.com>
2023-05-15 14:29:04 +0400

    find product with id stripe

--- BACK/videodriveapi/src/produits/produits.service.ts
+++ BACK/videodriveapi/src/produits/produits.service.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { CreateProduitDto } from './dto/create-produit.dto';
 import { UpdateProduitDto } from './dto/update-produit.dto';
 import Stripe from 'stripe';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Produit } from './entities/produit.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProduitsService {
-  constructor() {
+  constructor(@InjectRepository(Produit) private produitRepo: Repository<Produit>) {
     this.stripe = new Stripe(process.env.SK_TEST_STRIPE, { apiVersion: '2022-11-15' })
   }
   private stripe: Stripe;
@@ -17,7 +20,21 @@ export class ProduitsService {
   }
 
   findOne(idProduct: string) {
-    return this.stripe.products.retrieve(idProduct, { expand: ['data.default_price'] });;
+    return this.produitRepo.findOne({ where: { idStripe: idProduct } }).then(
+      produitDB => {
+        return this.stripe.products.retrieve(produitDB.idStripe,).then(
+          product => {
+            return this.stripe.prices.retrieve(product.default_price.toString()).then(
+              price => {
+                const produit = { ...product, prix: price, description: produitDB.description }
+                return produit;
+              }
+            )
+          }
+        );
+      }
+    )
+
   }
 
 }
